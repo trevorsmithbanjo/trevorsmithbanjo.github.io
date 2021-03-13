349b272638654f92fc202358bd57c7af34b2f2a7
Trevor Smith <trevorsmithbanjo@gmail.com>
2021-03-13 16:55:20 -0600

    Added Color Palette to tailwind.config.
    Also playing with a gradient background.

--- rebuild/src/App.js
+++ rebuild/src/App.js
@@ -4,7 +4,7 @@ import Nav from "./components/Nav";
 
 function App() {
   return (
-    <div>
+    <div className="w-full h-screen bg-gradient-to-r from-sunglow-500 via-royalblue-300 to-tomato-100">
       <Nav />
       <Header />
     </div>

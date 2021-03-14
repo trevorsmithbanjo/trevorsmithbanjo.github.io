cf2e5008e40aa7c356f99af452737ae0cdfedb34
Trevor Smith <trevorsmithbanjo@gmail.com>
2021-03-13 21:57:27 -0600

    Added Aside And Grid To Hero Component. Changed Some Colors Too.

--- rebuild/src/components/Header.js
+++ rebuild/src/components/Header.js
@@ -2,7 +2,7 @@ import React from "react";
 
 function Header() {
     return (
-        <div className="flex justify-center w-full h-10 pt-5 text-4xl p-12 m-12 border-2 border-red-500 container mx-auto uppercase font-bold">
+        <div className="flex justify-center w-full text-4xl p-10 uppercase font-bold">
             <h1>Trevor Smith</h1>
         </div>
     )
--- rebuild/src/components/Hero.js
+++ rebuild/src/components/Hero.js
@@ -3,17 +3,17 @@ import Headshot from "../images/headshot_small_copy.jpg";
 
 function Hero() {
     return (
-        <div className="border-2 border-red-500 w-full h-screen grid grid-cols-3 mr-2">
-            <main className="flex h-full col-span-2 border-2 border-red-500 overflow-hidden">
-                <div className="border-2 border-red-500">
-                    <img src={Headshot} alt="Headshot of Trevor Smith" className="max-w-sm"></img>
+        <div className="w-full h-full grid grid-cols-3 gap-4">
+            <main className="flex h-full col-span-2 overflow-hidden rounded bg-sunglow-50 shadow">
+                <div className="">
+                    <img src={Headshot} alt="Headshot of Trevor Smith" className="max-w-xs object-contain rounded"></img>
                 </div>
-                <div className="border-2 border-red-500 w-full">
+                <div className="w-full m-5">
                     <h3 className="text-4xl font-semibold">Developer</h3>
-                    <p></p>
+                    <p>Full Stack Web Developer with an extensive background in the music industry. Graduate of the University of Texas Coding Boot Camp with skills in HTML, CSS, JavaScript, and MERN stack. As a Grammy nominated musician I understand the importance of mastering skills through practice and perseverance. I’ve found similarities between coding and learning an instrument, as they are both challenging yet every small victory is more rewarding than the last. I am now able to apply this passion to my work as a programmer. Eager to combine my background in music with my technical skills to help the developer team build, test and deploy high quality products.</p>
                 </div>
             </main>
-            <aside className="col-span-1 w-full h-full border-2 border-blue-500">
+            <aside className="col-span-1 w-full h-full rounded bg-sunglow-50 shadow">
 
             </aside>
         </div>

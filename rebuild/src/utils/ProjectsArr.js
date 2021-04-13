5f23128d3fc02dbd10ddba864779a62ea3b48643
Trevor Smith <trevorsmithbanjo@gmail.com>
2021-04-13 17:25:40 -0500

    Fixes Typo On ProjectsArr.js.

--- rebuild/src/utils/ProjectsArr.js
+++ rebuild/src/utils/ProjectsArr.js
@@ -1,30 +1,33 @@
 export const ProjectsArr = [
-    {
-        image: "../images/ResuMake_screenshot.png",
-        title: "ResueMake",
-        summary: "Want to put your best foot forward and impress employers? Our resume helper can help to create a professional and sleek Resume as well as a Webpage within minutes. Try it out by creating an account with us!",
-        link: "https://resumake-pro.herokuapp.com/",
-        repo: "https://github.com/UT-final-project/group1-final-project"
-    },
-    {
-        image: "../images/Yay-or-Nay-Sign-Up-screenshot.png",
-        title: "Yay Or Nay",
-        summary: "Yay Or Nay is an application that is a dashboard for an employer to offer a place for employees to vote on ideas. For example where to get lunch on Friday. This is a group project that was build from the ground up using Node.js, Express.js and Sequelize in the band end as well as EJS for templating in the fron end.",
-        link: "https://p002-yay-or-nay.herokuapp.com/",
-        repo: "https://github.com/trevorsmithbanjo/p2_org_decider"
-    },
-    {
-        image: "../images/Fitness-Chart-screenshot.png",
-        title: "Track-Fit",
-        summary: "A fitness tracker application using node and express in the backend as well as mongoose with mongoDB for handling the database and database Schema.",
-        link: "https://trevorsmith-fit-track.herokuapp.com/",
-        repo: "https://github.com/trevorsmithbanjo/track-fit"
-    },
-    // {
-    //     image: "../images/Rescue-Spot-.png",
-    //     title: "Rescue \"Spot\"",
-    //     summary: "This is an application that will connect a user looking to adopt a dog to a list of dogs available to adopt based on their location. The UI is built with jQuery and the data is being pulled from the Petfinder API.",
-    //     link: "https://dcastelline.github.io/Project-1-Rescue-Spot/",
-    //     repo: "https://github.com/dcastelline/Project-1-Rescue-Spot"
-    // },
+  {
+    image: "../images/ResuMake_screenshot.png",
+    title: "ResueMake",
+    summary:
+      "Want to put your best foot forward and impress employers? Our resume helper can help to create a professional and sleek Resume as well as a Webpage within minutes. Try it out by creating an account with us!",
+    link: "https://resumake-pro.herokuapp.com/",
+    repo: "https://github.com/UT-final-project/group1-final-project",
+  },
+  {
+    image: "../images/Yay-or-Nay-Sign-Up-screenshot.png",
+    title: "Yay Or Nay",
+    summary:
+      "Yay Or Nay is an application that is a dashboard for an employer to offer a place for employees to vote on ideas. For example where to get lunch on Friday. This is a group project that was build from the ground up using Node.js, Express.js and Sequelize in the back end as well as EJS for templating in the front end.",
+    link: "https://p002-yay-or-nay.herokuapp.com/",
+    repo: "https://github.com/trevorsmithbanjo/p2_org_decider",
+  },
+  {
+    image: "../images/Fitness-Chart-screenshot.png",
+    title: "Track-Fit",
+    summary:
+      "A fitness tracker application using node and express in the backend as well as mongoose with mongoDB for handling the database and database Schema.",
+    link: "https://trevorsmith-fit-track.herokuapp.com/",
+    repo: "https://github.com/trevorsmithbanjo/track-fit",
+  },
+  // {
+  //     image: "../images/Rescue-Spot-.png",
+  //     title: "Rescue \"Spot\"",
+  //     summary: "This is an application that will connect a user looking to adopt a dog to a list of dogs available to adopt based on their location. The UI is built with jQuery and the data is being pulled from the Petfinder API.",
+  //     link: "https://dcastelline.github.io/Project-1-Rescue-Spot/",
+  //     repo: "https://github.com/dcastelline/Project-1-Rescue-Spot"
+  // },
 ];
